d4302a23b42a68ca953e71f4b6fad3af6cc42d90
estebanmasaya <estebanmasaya@gmail.com>
2024-04-17 10:58:17 +0200

    playing testkannal with baseurl for segment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import AudioPlayer from "./AudioPlayer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sveriges Radio Testkanal</h1>
+      <AudioPlayer src="https://strcl-cdn.sr.se/lc/p4plus" />
+
+
+
     </div>
   );
 }
---
+++ src/AudioPlayer.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useRef, useState} from 'react';
+import Hls from 'hls.js';
+import { v4 as uuidv4 } from 'uuid'; // Import the UUID library to generate a session ID
+
+const AudioPlayer = ({ src }) => {
+    const audioRef = useRef(null);
+    const [url, setUrl] = useState("testURl");
+    useEffect(() => {
+        const audio = audioRef.current;
+        let hls;
+
+        if (audio) {
+            if (Hls.isSupported()) {
+                // Generate a unique session ID for CMCD
+                const sessionId = uuidv4();
+                hls = new Hls({
+                    cmcd: {
+                        sessionId: sessionId, // Use the generated session ID
+                        contentId: 'your-content-id', // Replace with your actual content ID
+                        useHeaders: false,
+                    }
+                });
+                hls.loadSource(src);
+                hls.attachMedia(audio);
+                hls.on(Hls.Events.MANIFEST_PARSED, function () {
+                    audio.play();
+                });
+                hls.on(Hls.Events.FRAG_LOADING, (event, { frag }) => {
+                    if (frag.type === 'main') {
+                        console.log(`Loading fragment ${frag.sn} at quality level ${frag.level}`);
+                        setUrl(frag.sn + " " + frag.baseurl);
+                    }
+                });
+            } else if (audio.canPlayType('application/vnd.apple.mpegurl')) {
+                audio.src = src;
+                audio.addEventListener('loadedmetadata', function () {
+                    audio.play();
+                });
+            }
+        }
+
+        return () => {
+            if (hls) {
+                hls.destroy();
+            }
+        };
+    }, [src]);
+
+    return (
+        <dif>
+            <audio ref={audioRef} controls></audio>
+            <h3> {url} </h3>
+        </dif>
+
+    );
+};
+
+export default AudioPlayer;
