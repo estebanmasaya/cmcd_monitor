95afaced26b67f92f3fa4bd4c99a50a3d18124a0
Saba Jarwalli <saba.jarwalli@stryda.gg>
2024-04-26 12:54:56 +0200

    users can now enter URL
    You can now enter another livestream other then the SR test chanel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
-import AudioPlayer from "./AudioPlayer";
+import AudioPlayer from './AudioPlayer';
 
 function App() {
+  const [channelUrl, setChannelUrl] = useState('https://strcl-cdn.sr.se/lc/p4plus');
+
+  const handleInputChange = (event) => {
+    setChannelUrl(event.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>Sveriges Radio Testkanal</h1>
-      <AudioPlayer src="https://strcl-cdn.sr.se/lc/p4plus" />
-
-
-
+      <h1 className="title">Sveriges Radio</h1>
+      <div className="channel-input-container">
+        <label htmlFor="channelInput" className="channel-label">Enter Channel URL:</label>
+        <input
+          id="channelInput"
+          type="text"
+          value={channelUrl}
+          onChange={handleInputChange}
+          className="channel-input"
+          placeholder="Enter channel URL..."
+        />
+      </div>
+      <AudioPlayer src={channelUrl} />
     </div>
   );
 }
